dbf63325c4d2638328972fd7c24cd07a3edfb79c
Jordan Murphy <murphyjd541@gmail.com>
2020-04-02 15:13:33 -0500

    Set up Delete View

--- leave-management/Controllers/LeaveTypesController.cs
+++ leave-management/Controllers/LeaveTypesController.cs
@@ -128,23 +128,43 @@ namespace leave_management.Controllers
         // GET: LeaveTypes/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            if (!_repo.isExists(id))
+            {
+                return NotFound();
+            }
+
+            var leaveType = _repo.FindById(id);
+            var model = _mapper.Map<LeaveTypeViewModel>(leaveType);
+            return View(model);
         }
 
         // POST: LeaveTypes/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, LeaveHistoryViewModel model)
         {
             try
             {
                 // TODO: Add delete logic here
+                var leaveType = _repo.FindById(id);
+
+                if (leaveType == null)
+                {
+                    return NotFound();
+                }
+
+                var isSuccess = _repo.Delete(leaveType);
+
+                if (!isSuccess)
+                {                   
+                    return View(model);
+                }
 
                 return RedirectToAction(nameof(Index));
             }
             catch
-            {
-                return View();
+            {                
+                return View(model);
             }
         }
     }
