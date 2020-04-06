ea611f83a04fae621339a88d7b147e82e0e8feb7
Jordan Murphy <murphyjd541@gmail.com>
2020-04-06 16:48:06 -0500

    Added display name for AdminLeaveRequestViewViewModel

--- leave-management/Models/LeaveRequestViewModel.cs
+++ leave-management/Models/LeaveRequestViewModel.cs
@@ -49,12 +49,16 @@ namespace leave_management.Models
 
     public class AdminLeaveRequestViewViewModel
     {
+        [Display(Name = "Total Number of Requests")]
         public int TotalRequests { get; set; }
 
+        [Display(Name = "Pending Requests")]
         public int PendingRequests { get; set; }
 
+        [Display(Name = "Approved Requests")]
         public int ApprovedRequests { get; set; }
 
+        [Display(Name = "Rejected Requests")]
         public int RejectedRequests { get; set; }
 
         public List<LeaveRequestViewModel> LeaveRequests { get; set; }
